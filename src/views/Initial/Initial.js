9cfd51dd5323e0f4a3d7ff8fc8cafe60dddfc248
Alexander Mol <azertyalex@hotmail.com>
2019-07-12 11:33:58 +0200

    auto fill in name if known

--- src/views/Initial/Initial.js
+++ src/views/Initial/Initial.js
@@ -23,7 +23,9 @@ export default {
     }
   },
   mounted () {
-    console.log(this.$route)
+    var tempName = localStorage.getItem("username")
+    if(tempName) this.doubleName = tempName.split('.')[0]
+
     if (this.$route.query.logintoken && this.$route.query.doublename) {
       this.doubleName = this.$route.query.doublename
       this.setDoubleName(this.$route.query.doublename)
